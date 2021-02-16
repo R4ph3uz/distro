67c01c17d40c33d6dae7988523bca760bdc32def
R4ph3uz <65567704+R4ph3uz@users.noreply.github.com>
2021-02-16 21:43:58 +0100

    Update install.sh

--- v2/install.sh
+++ v2/install.sh
@@ -2,7 +2,10 @@
 sudo update.sh
 
 sudo setup_system.sh
+# create cybersec and cp wordlists + cp wallpaper
 
 sudo install_apps.sh
+# install FFdev + discord + terminator + zsh + disks
 
 sudo setup_config.sh
+# config ffdev + terminator and zsh/oh-my-zsh
