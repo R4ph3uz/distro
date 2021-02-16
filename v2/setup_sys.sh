ec149c3629949bf0810079c764f2c73b0e923920
R4ph3uz <65567704+R4ph3uz@users.noreply.github.com>
2021-02-16 08:54:05 +0100

    add sudo

--- v2/setup_sys.sh
+++ v2/setup_sys.sh
@@ -1,5 +1,5 @@
 export TERM=xterm-256color
 export PATH=/cybersec:~/opt:$PATH
 
-mkdir -p /cybersec/wordlists
-cp /usr/share/wordlists/rockyou.txt.gz /cybersec/wordlists
+sudo mkdir -p /cybersec/wordlists
+sudo cp /usr/share/wordlists/rockyou.txt.gz /cybersec/wordlists
