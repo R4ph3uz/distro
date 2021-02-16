943b163ab369a4fae55c20ae0ea88c0257e65325
R4ph3uz <65567704+R4ph3uz@users.noreply.github.com>
2021-02-16 09:01:40 +0100

    cp wallpaper

--- v2/setup_sys.sh
+++ v2/setup_sys.sh
@@ -3,3 +3,5 @@ export PATH=/cybersec:~/opt:$PATH
 
 sudo mkdir -p /cybersec/wordlists
 sudo cp /usr/share/wordlists/rockyou.txt.gz /cybersec/wordlists
+
+sudo cp ./bg/{blue-to-pink.jpg,blue-to-purple.jpg,orange-to-pink.jpg,purple-to-yellow.jpg} /usr/share/backgrounds/
