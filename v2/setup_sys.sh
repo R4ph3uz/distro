acd8f799f00dbfeb679381337577f578b269658c
R4ph3uz <65567704+R4ph3uz@users.noreply.github.com>
2021-03-03 20:59:12 +0100

    setup icon terminator

--- v2/setup_sys.sh
+++ v2/setup_sys.sh
@@ -6,3 +6,20 @@ sudo mkdir -p /cybersec/wordlists
 sudo cp /usr/share/wordlists/rockyou.txt.gz /cybersec/wordlists
 
 sudo cp ./bg/{blue-to-pink.jpg,blue-to-purple.jpg,orange-to-pink.jpg,purple-to-yellow.jpg} /usr/share/backgrounds/
+
+sudo echo "Name[fr]=Terminator
+Comment[fr]=Permet d'avoir plusieurs terminaux en une seule fenêtre
+TryExec=terminator
+Exec=terminator
+Icon=/usr/share/icons/maia/apps/scalable/terminator-128.svg
+Type=Application
+Categories=GNOME;GTK;Utility;TerminalEmulator;System;
+StartupNotify=true
+X-Ubuntu-Gettext-Domain=terminator
+X-Ayatana-Desktop-Shortcuts=NewWindow;
+Keywords=terminal;shell;prompt;command;commandline;
+[NewWindow Shortcut Group]
+Name=Open a New Window
+Exec=terminator
+TargetEnvironment=Unity
+" > /usr/share/applications/terminator.desktop
